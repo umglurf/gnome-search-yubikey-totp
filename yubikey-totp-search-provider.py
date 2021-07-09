2e820ff888444143bca4bd7c6ad3b9a89a44df2a
Håvard Moen <post@haavard.name>
2021-07-09 06:34:28 +0200

    only show in search if starting with totp, code or mfa

--- yubikey-totp-search-provider.py
+++ yubikey-totp-search-provider.py
@@ -27,54 +27,57 @@ DBusGMainLoop(set_as_default=True)
 
 # Convenience shorthand for declaring dbus interface methods.
 # s.b.n. -> search_bus_name
-search_bus_name = 'org.gnome.Shell.SearchProvider2'
+search_bus_name = "org.gnome.Shell.SearchProvider2"
 sbn = dict(dbus_interface=search_bus_name)
 
+
 class SearchPassService(dbus.service.Object):
-    """ The search daemon.
+    """The search daemon.
     This service is started through DBus activation by calling the
     :meth:`Enable` method, and stopped with :meth:`Disable`.
     """
-    bus_name = 'name.haavard.Yubikey.SearchProvider'
 
-    _object_path = '/' + bus_name.replace('.', '/')
+    bus_name = "name.haavard.Yubikey.SearchProvider"
+
+    _object_path = "/" + bus_name.replace(".", "/")
 
     def __init__(self):
         self.session_bus = dbus.SessionBus()
         bus_name = dbus.service.BusName(self.bus_name, bus=self.session_bus)
         dbus.service.Object.__init__(self, bus_name, self._object_path)
 
-    @dbus.service.method(in_signature='sasu', **sbn)
+    @dbus.service.method(in_signature="sasu", **sbn)
     def ActivateResult(self, id, terms, timestamp):
-        subprocess.run([
-            "yubikey-totp-copy-code.py",
-            ''.join(terms)
-            ])
+        subprocess.run(["yubikey-totp-copy-code.py", "".join(terms[1:])])
 
-    @dbus.service.method(in_signature='as', out_signature='as', **sbn)
+    @dbus.service.method(in_signature="as", out_signature="as", **sbn)
     def GetInitialResultSet(self, terms):
-        print(f"get resultset {terms}")
         return self.get_result_set(terms)
 
-    @dbus.service.method(in_signature='as', out_signature='aa{sv}', **sbn)
+    @dbus.service.method(in_signature="as", out_signature="aa{sv}", **sbn)
     def GetResultMetas(self, ids):
         return [dict(id=id, name=id, gicon="com.yubico.yubioath") for id in ids]
 
-    @dbus.service.method(in_signature='asas', out_signature='as', **sbn)
+    @dbus.service.method(in_signature="asas", out_signature="as", **sbn)
     def GetSubsearchResultSet(self, previous_results, new_terms):
         return self.get_result_set(new_terms)
 
-    @dbus.service.method(in_signature='asu', terms='as', timestamp='u', **sbn)
+    @dbus.service.method(in_signature="asu", terms="as", timestamp="u", **sbn)
     def LaunchSearch(self, terms, timestamp):
         pass
 
     def get_result_set(self, terms):
-        name = ''.join(terms)
-        return [f"Get oauth code for {name}"]
+        if len(terms) > 1 and terms[0] in ("code", "mfa", "totp"):
+            name = "".join(terms[1:])
+            return [f"Get oauth code for {name}"]
+        else:
+            return []
+
 
 def main():
     SearchPassService()
     GLib.MainLoop().run()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
